701d3cba6d4cf8db6c9262b4052b627760929537
yennym3 <yennylm4@gmail.com>
2024-09-25 18:10:37 +0200

    deployment-kne

--- Topology/Network/configuration/gateway2-config.py
+++ Topology/Network/configuration/gateway2-config.py
@@ -1,15 +1,8 @@
 import subprocess
 
-# Comando para agregar la direccion IP en la interfaz eth1
-comando_addr = "ip addr add 11.0.1.1/24 dev eth1"
-subprocess.run(comando_addr, shell=True)
-
 # Definir las rutas a agregar
 rutas = [
-    "10.0.1.0/24",
-    "12.12.12.0/24",
-    "192.168.6.0/24", 
-    "192.168.159.0/24",
+    "11.0.1.0/24",
     "192.168.1.0/24",
     "192.168.2.0/24",
     "192.168.3.0/24",
@@ -23,14 +16,16 @@ rutas = [
     "192.168.11.0/24",
     "192.168.12.0/24",
     "192.168.13.0/24",
-    "192.168.14.0/24",
-    "192.168.15.0/24",  
-    "192.168.16.0/24"
+    "192.168.14.0/24", 
+    "192.168.15.0/24", 
+    "192.168.16.0/24" 
 ]
 
 # Puerta de enlace y nombre de interfaz comunes para todas las rutas
-puerta_enlace = "11.0.1.3"
+puerta_enlace = "12.12.12.2"
+puerta_enlace_eth2 = "10.0.1.15"
 interfaz = "eth1"
+
 
 #Eliminar y agregar ruta por defecto 
 ruta_por_defecto = "default"
@@ -38,7 +33,7 @@ ruta_por_defecto = "default"
 # Comando para verificar si la ruta por defecto ya existe
 comando_verificar_ruta = f"ip route show | grep {ruta_por_defecto}"
 
-# Ejecutar el comando de verificación
+# Ejecutar el comando de verificaci√≥n
 resultado = subprocess.run(comando_verificar_ruta, shell=True, stdout=subprocess.PIPE)
 
 # Si la ruta por defecto ya existe, la eliminamos
@@ -47,11 +42,9 @@ if resultado.returncode == 0:
     subprocess.run(comando_eliminar_ruta, shell=True)
 
 # Agregar la ruta por defecto
-comando_ruta_por_defecto = f"ip route add {ruta_por_defecto} via {puerta_enlace} dev {interfaz}"
+comando_ruta_por_defecto = f"ip route add {ruta_por_defecto} via {puerta_enlace_eth2} dev eth2"
 subprocess.run(comando_ruta_por_defecto, shell=True)
 
-# Agregar la direccion IP en eth1
-subprocess.run(comando_addr, shell=True)
 
 # Agregar cada ruta utilizando subprocess
 for ruta in rutas:
--- Topology/Network/configuration/server-config.py
+++ Topology/Network/configuration/server-config.py
@@ -1,13 +1,20 @@
 import subprocess
 
+# Comando para agregar la direccion IP en la interfaz eth1
+comando_addr = "ip addr add 11.0.1.1/24 dev eth1"
+subprocess.run(comando_addr, shell=True)
+
 # Definir las rutas a agregar
 rutas = [
-    "192.168.6.0/24",
+    "10.0.1.0/24",
+    "12.12.12.0/24",
+    "192.168.6.0/24", 
     "192.168.1.0/24",
     "192.168.2.0/24",
     "192.168.3.0/24",
     "192.168.4.0/24",
     "192.168.5.0/24",
+    "192.168.6.0/24",
     "192.168.7.0/24",
     "192.168.8.0/24",
     "192.168.9.0/24",
@@ -15,16 +22,14 @@ rutas = [
     "192.168.11.0/24",
     "192.168.12.0/24",
     "192.168.13.0/24",
-    "192.168.14.0/24", 
-    "10.10.10.0/24",
-    "192.168.159.0/24"
+    "192.168.14.0/24",
+    "192.168.15.0/24",  
+    "192.168.16.0/24"
 ]
 
 # Puerta de enlace y nombre de interfaz comunes para todas las rutas
-puerta_enlace = "10.10.10.2"
-puerta_enlace_eth2 = "192.168.159.1"
+puerta_enlace = "11.0.1.3"
 interfaz = "eth1"
-
 
 #Eliminar y agregar ruta por defecto 
 ruta_por_defecto = "default"
@@ -32,7 +37,7 @@ ruta_por_defecto = "default"
 # Comando para verificar si la ruta por defecto ya existe
 comando_verificar_ruta = f"ip route show | grep {ruta_por_defecto}"
 
-# Ejecutar el comando de verificaci√≥n
+# Ejecutar el comando de verificación
 resultado = subprocess.run(comando_verificar_ruta, shell=True, stdout=subprocess.PIPE)
 
 # Si la ruta por defecto ya existe, la eliminamos
@@ -41,9 +46,11 @@ if resultado.returncode == 0:
     subprocess.run(comando_eliminar_ruta, shell=True)
 
 # Agregar la ruta por defecto
-comando_ruta_por_defecto = f"ip route add {ruta_por_defecto} via {puerta_enlace_eth2} dev eth2"
+comando_ruta_por_defecto = f"ip route add {ruta_por_defecto} via {puerta_enlace} dev {interfaz}"
 subprocess.run(comando_ruta_por_defecto, shell=True)
 
+# Agregar la direccion IP en eth1
+subprocess.run(comando_addr, shell=True)
 
 # Agregar cada ruta utilizando subprocess
 for ruta in rutas:
